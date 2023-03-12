d80f64059428936f1c124e719a4a1a70fff4f941
awarschauer <127681394+awarschauer@users.noreply.github.com>
2023-03-12 17:38:49 +0100

    Add files via upload

---
+++ grove_installeer.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Clone the grove.py repository
+git clone https://github.com/Seeed-Studio/grove.py
+
+# Change directory into the grove.py directory
+cd grove.py
+
+# Install the library using pip
+sudo pip install .
+
+# Install the library using pip3
+sudo pip3 install .
+
+# Clone the grove repository into the Desktop directory
+git clone https://github.com/awarschauer/grove.git ~/Desktop
+
+# Make all the .py files in the current directory executable by the owner
+chmod u+x *.py
+
